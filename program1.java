8a70f4b5a7f75c44fa1ddb9e667e4f4d5d7f2243
harika-1 <41731552+harika-1@users.noreply.github.com>
2018-07-27 09:57:28 +0530

    Create program1.java

---
+++ program1.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main(String args[])
+	{
+		int num;
+		Scanner sc=new Scanner(System.in);
+		num=sc.nextInt();
+		if(num>0)
+		{
+			System.out.println("Positive");
+	}
+	else if(num<0)
+	{
+		System.out.println("Negative");
+}
+else
+{
+	System.out.println("Zero");
+}
+}
+}
