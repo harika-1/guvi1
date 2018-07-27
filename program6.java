cfb78bf0b8ad0fa3ad7016adb481f7f8449ac192
harika-1 <41731552+harika-1@users.noreply.github.com>
2018-07-27 13:01:53 +0530

    Create program6.java

---
+++ program6.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class LeapYear
+{
+	public static void main (String[] args)
+	{
+		int year=1900;
+		boolean leap=false;
+if(year%4==0)
+{
+System.out.println("Yes");
+else
+System.out.println(" No");
+}
+}
+}
