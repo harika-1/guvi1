f80cf743fde40fe00fe453283890261f4fa6287c
harika-1 <41731552+harika-1@users.noreply.github.com>
2018-07-27 14:31:46 +0530

    Create program8.java

---
+++ program8.java
@@ -0,0 +1,18 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args)
+	{
+		int n,r;
+		Scanner S=new Scanner(System.in);
+		n=S.nextInt();
+		r=(n*((n+1)/2));
+		System.out.println(r);
+	}
+}
