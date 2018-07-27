6559bb185adb27cd7c86d98a225397e573e94931
harika-1 <41731552+harika-1@users.noreply.github.com>
2018-07-27 12:27:05 +0530

    Create program5.java

---
+++ program5.java
@@ -0,0 +1,18 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Alphabet
+{
+	public static void main (String[] args)
+	{
+	char c='*';
+	if((c>='a' && c<='z')||(c>='A' && c<='z'))
+	System.out.println("Alphabet");
+	else
+	System.out.println("No Alphabet");
+	}
+}
